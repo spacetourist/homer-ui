f8120a991625b96547df71ce92916c2e82df1bc3
Callum <callum.guy@x-on.co.uk>
2022-12-15 23:57:17 +0000

    perform download save

--- src/app/services/agentsub.service.ts
+++ src/app/services/agentsub.service.ts
@@ -1,71 +1,92 @@
-import { HttpGetBuffer } from '@app/helpers/http-get-buffer';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '@environments/environment';
-import { PreferenceAgentsub } from '@app/models';
+import {HttpGetBuffer} from '@app/helpers/http-get-buffer';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '@environments/environment';
+import {PreferenceAgentsub} from '@app/models';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AgentsubService {
 
-    private url = `${environment.apiUrl}/agent`;
+  private url = `${environment.apiUrl}/agent`;
 
-    constructor(private http: HttpClient, private httpGetBuffer: HttpGetBuffer) { }
+  constructor(private http: HttpClient, private httpGetBuffer: HttpGetBuffer) {
+  }
 
-    // Agentsub protokols
-    getProtokols(): Observable<PreferenceAgentsub> {
-        return this.httpGetBuffer.get<PreferenceAgentsub>(`${this.url}/subscribe`);
+  // Agentsub protokols
+  getProtokols(): Observable<PreferenceAgentsub> {
+    return this.httpGetBuffer.get<PreferenceAgentsub>(`${this.url}/subscribe`);
+  }
+
+  // Agentsub fields
+  getFields(): Observable<PreferenceAgentsub> {
+    return this.http.get<PreferenceAgentsub>(`${this.url}/fields`);
+  }
+
+  /**
+   * Perform lookup for data type against HEPSUB subscriber UUID
+   *
+   * i.e.
+   *  search:   http://192.168.181.241:9080/api/v3/agent/search/06a96780-7739-11ed-91e0-fbabc1036a33/pcap
+   *  download: http://192.168.181.241:9080/api/v3/agent/search/06a96780-7739-11ed-91e0-fbabc1036a33/download
+   */
+  getHepsubElements({uuid, type, data}): Observable<any> {
+    // todo consider returning .toPromise() as per call.service
+    if (type === 'download') {
+      const res = this.http.post<any>(`${this.url}/search/${uuid}/${type}`, data).subscribe(
+        (response: any) =>{
+          let filename = 'homer_rtp.pcap';
+          let dataType = response.type;
+          let binaryData = [];
+          binaryData.push(response);
+          let downloadLink = document.createElement('a');
+          downloadLink.href = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+          if (filename) {
+            downloadLink.setAttribute('download', filename);
+          }
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          return res;
+        }
+      );
     }
+    return this.http.post<any>(`${this.url}/search/${uuid}/${type}`, data);
+  }
 
-    // Agentsub fields
-    getFields(): Observable<PreferenceAgentsub> {
-        return this.http.get<PreferenceAgentsub>(`${this.url}/fields`);
-    }
+  // type = 'cdr' | 'wav' | 'json'
+  getType(type: string): Observable<any> {
+    return this.http.get<any>(`${this.url}/type/${type}`);
+  }
+
+  getAgentCdr(type): Observable<any> {
     /**
-     * Perform lookup for data type against HEPSUB subscriber UUID
-     *
-     * i.e.
-     *  search:   http://192.168.181.241:9080/api/v3/agent/search/06a96780-7739-11ed-91e0-fbabc1036a33/pcap
-     *  download: http://192.168.181.241:9080/api/v3/agent/search/06a96780-7739-11ed-91e0-fbabc1036a33/download
+     * TODO:
+     * transaction dialog / logs / HEPSUB:"test-endpoint"/ cdr
+     * as on HOMER [domain:port-1]/dashboard/home
+     * [domain:port-1]/api/v3/agent/type/cdr
      */
-    getHepsubElements({ uuid, type, data }): Observable<any> {
-        return this.http.post<any>(`${this.url}/search/${uuid}/${type}`, data);
+    return this.http.get(`${this.url}/type/${type}`);
+
+  }
+
+  getData(agent: any, type?: string): Observable<any> {
+    // console.log({agent, type});
+    const {protocol, host, port, path} = agent;
+    const returnDefault = () => new Observable<any>(observer => {
+      observer.next(null);
+      observer.complete();
+    });
+    if (protocol !== 'http' && protocol !== 'https') {
+      return returnDefault();
     }
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-    // type = 'cdr' | 'wav' | 'json'
-    getType(type: string): Observable<any> {
-        return this.http.get<any>(`${this.url}/type/${type}`);
+    try {
+      return this.http.post(`${protocol}://${host}:${port}${path}/${type}`, {headers});
+    } catch (err) {
+      return returnDefault();
     }
-
-    getAgentCdr(type): Observable<any> {
-        /**
-         * TODO:
-         * transaction dialog / logs / HEPSUB:"test-endpoint"/ cdr
-         * as on HOMER [domain:port-1]/dashboard/home
-         * [domain:port-1]/api/v3/agent/type/cdr
-         */
-        return this.http.get(`${this.url}/type/${type}`);
-
-    }
-
-    getData(agent: any, type?: string): Observable<any> {
-        // console.log({agent, type});
-        const { protocol, host, port, path } = agent;
-        const returnDefault = () => new Observable<any>(observer => {
-            observer.next(null);
-            observer.complete();
-        });
-        if (protocol !== 'http' && protocol !== 'https') {
-            return returnDefault();
-        }
-        const headers = new HttpHeaders().set('Content-Type', 'application/json');
-
-        try {
-            return this.http.post(`${protocol}://${host}:${port}${path}/${type}`, { headers });
-        } catch (err) {
-            return returnDefault();
-        }
-    }
+  }
 }
